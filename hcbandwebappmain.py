9b1783b31e1c92ecd2e178fa057b1ca6c8fb13bd
rchrdlln <rchrdlln@gmail.com>
2016-02-09 00:10:48 -0500

    new flask main

---
+++ hcbandwebappmain.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+from flask import render_template
+app = Flask(__name__)
+@app.route('/') 
+
+# def hello_world():
+# 	return 'Hello World!'
+
+def hello():
+	return render_template('template1.html')	
+	
+if __name__ == '__main__': 
+	app.run()
